b4756b8174a3fed4bd3a0e8d5cdd26a8cbc3094d
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-03-11 14:55:35 -0700

    add Id property and private list field and add them to Constructor

--- PlacesBlog/Models/Place.cs
+++ PlacesBlog/Models/Place.cs
@@ -1,14 +1,19 @@
 using System;
+using System.Collections.Generic;
 
 namespace PlacesBlog.Models
 {
   public class Place
   {
     public string CityName { get; set; }
+    public int Id { get; }
+    private List<Place> _allPlaces = new List<Place>{};
 
     public Place(string cityName)
     {
       CityName = cityName;
+      _allPlaces.Add(this);
+      Id = _allPlaces.Count;
     }
   }
 }
